80da111b92a4e53a851a930b3712152e99b00dc6
Larry Lu <pudding850806@gmail.com>
2017-12-04 13:15:50 +0800

    Use es6 syntax

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
 // reply to request with "Hello World!"
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-//start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
-
-  var port = server.address().port;
-  console.log('Example app listening on port ', port);
-
-});
+// start a server on port 8080
+const server = app.listen(8080, () => {
+  const port = server.address().port
+  console.log('Example app listening on port ', port)
+})
